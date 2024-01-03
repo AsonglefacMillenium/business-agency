f2c590ab17913c38fb547c0ff159dd44638bb26b
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-01-03 22:56:31 +0100

    add icons

---
+++ components/atoms/icons/index.tsx
@@ -0,0 +1,9 @@
+export { default as BrainIcon } from "./brain-icon";
+export { default as DiamondIcon } from "./diamond-icon";
+export { default as EmailIcon } from "./email-icon";
+export { default as FilterIcon } from "./filter-icon";
+export { default as PenIcon } from "./pen-icon";
+export { default as PersonIcon } from "./person-icon";
+export { default as SoundIcon } from "./soundIcon";
+export { default as TestIcon } from "./test-icon";
+export { default as TorchIcon } from "./torch-icon";
---
+++ components/atoms/icons/pen-icon/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PenIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+    >
+      <path
+        d="M10.4151 26.6413L2 30L5.35874 21.5849M10.4151 26.6413L27.9018 9.15454M10.4151 26.6413L9.74234 22.2566L5.35874 21.5849M5.35874 21.5849L22.8455 4.09819M27.9018 9.15454L22.8455 4.09819M27.9018 9.15454L29.4718 7.58452C30.8681 6.18825 30.8681 3.92445 29.4718 2.52817V2.52817C28.0756 1.1319 25.8118 1.1319 24.4155 2.52817L22.8455 4.09819"
+        stroke="#292556"
+        stroke-width="2.36842"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default PenIcon;
