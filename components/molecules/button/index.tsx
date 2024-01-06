650457a7034e852cdd9dfa6588129b8d57a238de
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-01-06 19:48:16 +0300

    add interface for serviceCard

--- components/molecules/button/index.tsx
+++ components/molecules/button/index.tsx
@@ -1,21 +1,29 @@
-"use client"
+"use client";
 
-import { Text } from '@/components/atoms'
-import { ArrowRightBlackIcon } from '@/components/atoms/icons'
-import React from 'react'
+import { Text } from "@/components/atoms";
+import { ArrowRightBlackIcon } from "@/components/atoms/icons";
+import React from "react";
 
-const Button = ({...props}: IButtonProp) => {
+const Button = ({ ...props }: IButtonProp) => {
   return (
-    <button className='flex items-center p-[10px] rounded-2xl gap-[10px] ' color={props.bgColor} style={{
-        backgroundColor: props.bgColor
-    }}
-    onClick={props.onClick}
+    <button
+      className="flex items-center p-[10px] rounded-2xl gap-[10px] "
+      color={props.bgColor}
+      style={{
+        backgroundColor: props.bgColor,
+      }}
+      onClick={props.onClick}
     >
-        <ArrowRightBlackIcon />
+      <ArrowRightBlackIcon />
 
-        <Text text={props.text} color={props.color} fontSize={"small"} fontWeight={"bold"} />
+      <Text
+        text={props.text}
+        color={props.color}
+        fontSize={"small"}
+        fontWeight={"bold"}
+      />
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
---
+++ components/molecules/services-card/nterface.tsx
@@ -0,0 +1,4 @@
+interface IServiceProps {
+    text: string 
+    
+}
