406ca469bc3abc86c61c2dc0fb3b491cf64009f4
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-01-20 15:32:33 +0300

    add footer

---
+++ components/templates/footer/footer.data.tsx
@@ -0,0 +1,61 @@
+export const pageData = [
+  {
+    text: "Home",
+    route: "/",
+  },
+
+  {
+    text: "Home 2",
+    route: "/home2",
+  },
+
+  {
+    text: "About",
+    route: "/about",
+  },
+
+  {
+    text: "Contact Us",
+    route: "/contact",
+  },
+
+  {
+    text: "Portfolio",
+    route: "/portfolio",
+  },
+
+  {
+    text: "Portfolio Single",
+    route: "/portfolio2",
+  },
+];
+
+export const utilityPageData = [
+    
+        {
+            text: "Style Guide ",
+            route: "/styles"
+    
+        },
+
+        {
+            text: "Instruction ",
+            route: "/instruction"
+    
+        },
+
+        {
+            text: "License ",
+            route: "/license"
+    
+        },
+
+        {
+            text: "Changelog ",
+            route: "/changelog"
+    
+        },
+
+        
+    
+]
--- components/templates/footer/index.tsx
+++ components/templates/footer/index.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import { Text } from "@/components/atoms";
+import React from "react";
+import { pageData, utilityPageData } from "./footer.data";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-full bg-[#0C0C0C] pt-[100px] pb-[60px]">
+      <div className="w-[80%] mx-auto flex flex-col gap-[100px] ">
+        <div className="w-full flex justify-between flex-wrap">
+          <div className="w-[20%] flex flex-col gap-[24px]">
+            <Text
+              text="AGENCY"
+              color="#fff"
+              fontSize={"Xlarge"}
+              fontWeight={"bold"}
+            />
 
-export default Footer
+            <Text
+              text="We’re a team of strategic creator and digital innovator, united focus in our pursuit of mastery and joyful."
+              color="#fff"
+              fontSize={"normal"}
+              fontWeight={"400"}
+            />
+          </div>
+
+          <div className="flex flex-col gap-[24px]">
+            <Text
+              text="Pages"
+              color="#fff"
+              fontSize={"large"}
+              fontWeight={"400"}
+            />
+
+            <div className="flex flex-col gap-[16px]">
+              {pageData.map((data) => (
+                <Link href={data.route}>
+                   <Text
+                    text={data.text}
+                    color="#F4F4F4"
+                    fontSize={"small"}
+                    fontWeight={"400"}
+                  />
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-[24px]">
+            <Text
+              text="Pages"
+              color="#fff"
+              fontSize={"large"}
+              fontWeight={"400"}
+            />
+
+            <div className="flex flex-col gap-[16px]">
+              {utilityPageData.map((data) => (
+                <Link href={data.route}>
+                  <Text
+                    text={data.text}
+                    color="#F4F4F4"
+                    fontSize={"small"}
+                    fontWeight={"400"}
+                  />
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-[19px] w-[20%]">
+          <Text
+                    text="Subscribe"
+                    color="#F4F4F4"
+                    fontSize={"large"}
+                    fontWeight={"bold"}
+                  />
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
