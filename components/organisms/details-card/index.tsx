121325f5fc98c323dd654ce0e47c873dd8369388
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-01-29 22:55:25 +0100

    complete the details lander

--- components/organisms/details-card/index.tsx
+++ components/organisms/details-card/index.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 
 const DetailsCard = ({...props}: DetailsCardProps) => {
   return (
-    <div {...props}>
+    <div {...props} className='w-fit h-fit bg-[#0C0C0C] rounded-[15px] gap-[11px] px-[30px] py-[20px] flex flex-col justify-center ' >
         <Text 
           text={props.title}
-          color="#000"
+          color="#fff"
           fontWeight={"bold"}
           fontSize={"large"}
         />
         <Text
           text={props.text}
-          color="#000"
-          fontWeight={"semiBold"}
+          color="#fff"
+          fontWeight={"400"}
           fontSize={"normal"}
         /> 
     </div>
--- components/templates/details-lander/index.tsx
+++ components/templates/details-lander/index.tsx
@@ -1,9 +1,10 @@
+import { DetailsCard } from "@/components/organisms";
 import Image from "next/image";
 import React from "react";
 
 const DetailsLander = () => {
   return (
-    <div className="w-full bg-white py-[35px]">
+    <div className="w-full bg-white py-[35px] mb-[100px]">
       <div className="w-[70%] mx-auto  relative">
         <div className="w-full rounded-2xl h-[590px]">
           <Image
@@ -15,8 +16,11 @@ const DetailsLander = () => {
           />
         </div>
 
-        <div className="w-full flex absolute bottom-[5%] mb-[-30px] z-20 bg-[red] justify-center items-center gap-[45px]">
-
+        <div className="w-full flex absolute bottom-[0%] mb-[-30px] z-20 justify-center items-center gap-[45px]">
+          <DetailsCard text="Cloud solution" title="Category:" />
+          <DetailsCard text="BrightMedia Solutions" title="Client" />
+          <DetailsCard text="August 23, 2023" title="Date:" />
+          <DetailsCard text="489 Depot Road Midland" title="Location:" />
         </div>
       </div>
     </div>
