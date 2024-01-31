56f16155c1afa8673bb0a6e09591b15594599c85
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-01-31 23:58:39 +0300

    update contact screen

--- components/screens/contact-screen/index.tsx
+++ components/screens/contact-screen/index.tsx
@@ -1,8 +1,14 @@
+import { Header2 } from '@/components/organisms'
+import { Footer } from '@/components/templates'
 import React from 'react'
 
 const ContactScreen = () => {
   return (
-    <div>ContactScreen</div>
+    <div className="w-full p-0 m-0 pt-[67px]">
+    <Header2 />
+   
+    <Footer />
+  </div>
   )
 }
 
