7605c5b71bd4469b8e83490c5a9dfb387f94142e
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-02-01 23:45:30 +0300

    add contact form

--- components/organisms/index.tsx
+++ components/organisms/index.tsx
@@ -3,3 +3,4 @@ export { default as Header2 } from "./header2";
 export { default as StatsCard } from "./stats-card";
 export { default as TeamCard } from "./team-card";
 export { default as DetailsCard } from "./details-card";
+export { default as ContactForm } from "./contact-form";
