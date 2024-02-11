6a8587177b80d7be56d824d0860da340a1083038
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-02-11 22:54:03 +0300

    update the homepage

--- components/templates/mission-section/index.tsx
+++ components/templates/mission-section/index.tsx
@@ -8,13 +8,13 @@ import Image from "next/image";
 const MissionSection = () => {
   return (
     <div className="w-full py-[100px] bg-[#0C0C0C]">
-      <div className="lg:w-[80%] w-[90%] mx-auto flex lg:justify-between justify-center items-center">
+      <div className="lg:w-[80%] w-full mx-auto flex lg:justify-between justify-center items-center">
         <div className="lg:w-[45%] lg:visible invisible">
           <Image alt="" width={500} height={500} src="/images/mission.png" className="w-full"/>
         </div>
 
-        <div className="lg:w-[45%] w-[100%]  flex flex-col gap-8">
-          <div className="w-full gap-[25px] flex flex-col">
+        <div className="lg:w-[45%] w-full flex flex-col gap-8">
+          <div className="w-full gap-[25px] flex lg:items-start items-center flex-col">
             <Text
               color="#64CCC5"
               text="About Us"
