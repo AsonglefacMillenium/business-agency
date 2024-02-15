44b57e8cfeefd067b831b54c285b931187768aaa
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-02-15 23:01:53 +0300

    make portfolio page responsive

--- components/templates/portfolio-lander/index.tsx
+++ components/templates/portfolio-lander/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const PortfolioLander = () => {
   return (
-    <div className="w-[70%] mx-auto bg-[#0C0C0C] flex items-center justify-center h-[350px] rounded-2xl mt-[67px]">
+    <div className="lg:w-[70%] w-[95%] mx-auto bg-[#0C0C0C] flex items-center justify-center h-[350px] rounded-2xl mt-[67px]">
       <Text
         color="#FBFBFB"
         text="Our Portfolio"
